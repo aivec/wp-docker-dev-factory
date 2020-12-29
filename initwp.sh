9a3feaeaa66c4a687f7a7f670f7f7c51f33ab0d0
Evan Shaw <evandanielshaw@gmail.com>
2020-12-29 17:26:41 +0900

    fix: update images to use xdebug 3.0

--- initwp.sh
+++ initwp.sh
@@ -18,7 +18,7 @@ logger() {
 }
 
 # setting the XDEBUG_CONFIG environment variable doesn't seem to work so we hardcode it here
-sudo sh -c "echo 'xdebug.remote_host=${DOCKER_BRIDGE_IP}' >> /usr/local/etc/php/conf.d/xdebug.ini"
+sudo sh -c "echo 'xdebug.client_host=${DOCKER_BRIDGE_IP}' >> /usr/local/etc/php/conf.d/xdebug.ini"
 
 # PHP doesnt seem to pick up on environment variables when started via apache so we
 # have to explicitly list them for apache
